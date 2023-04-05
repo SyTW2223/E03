7a34476b1244aeef8a4cd4b9032d9dd6d5ae3c7d
Hector <alu010136107@ull.edu.es>
2023-04-05 12:04:53 +0100

    Modificacion busqueda de un usuario y validacion de la contraseña

--- my-project-server/index.js
+++ my-project-server/index.js
@@ -32,7 +32,7 @@ app.use(express.json())
 app.use(cors())
 
 const createLog = (req, res, next) => {
-  console.log(req.method, decodeURI(req.url), res.statusCode, req.body);
+  console.log(req.method, decodeURI(req.url), req.body);
   next();
 };
 
--- my-project-server/routes/login.js
+++ my-project-server/routes/login.js
@@ -1,7 +1,8 @@
 import express from "express"
 
-import Model from "../models/loginModel.js"
+import User from "../models/loginModel.js"
 import Joi from "@hapi/joi"
+import bcrypt from "bcrypt"
 
 const schemaLogin = Joi.object({
   username: Joi.string().required(),
@@ -22,16 +23,21 @@ router.post('/login', async (req, res) => {
       error: error.details[0].message
     })
   }
-  const data = new Model({
+
+  const data = new User({
     username: req.body.username,
     password: req.body.password,
   })
   // En caso de que la BBDD no este ON, salta el catch y lanzamos el error (10s)
   try {
-    const respuesta = await Model.findOne(data).exec()
-    if (respuesta != null) {
-      
-    }
+    const respuesta = await User.findOne({username: data.username}).exec()
+    // Si no se encuentra el usuario por el username
+    if (respuesta == null) return res.status(400).json({message: "Usuario no encontrado"})
+
+    const validPassword = await bcrypt.compare(req.body.password, User.password)
+    // Si la password no es igual a la del usuario guardada en la BBDD
+    if (!validPassword) return res.status(400).json({message: "Password invalida"})
+
     res.status(200).json(respuesta)
   } catch(error) {
     res.status(400).json({message: error.message})
