06db11c93d3d30bd94a193e6ba7ff90c814b3eec
Hector <alu010136107@ull.edu.es>
2023-04-05 19:48:20 +0100

    Comprobacion de la contraseña hasheada y no al realizar un login

--- my-project-server/routes/login.js
+++ my-project-server/routes/login.js
@@ -30,13 +30,11 @@ router.post('/login', async (req, res) => {
 
     const respuesta = await User.findOne({username: req.body.username}).exec()
     // Si no se encuentra el usuario por el username
-    console.log(respuesta)
     if (!respuesta) return res.status(400).json({message: "Usuario no encontrado"})
 
-    
-    //const validPassword = await bcrypt.compare(req.body.password, User.password)
-    // Si la password no es igual a la del usuario guardada en la BBDD
-    //if (!validPassword) return res.status(400).json({message: "Password invalida"})
+    const validPassword = await bcrypt.compare(req.body.password, respuesta.password)
+    //Si la password no es igual a la del usuario guardada en la BBDD
+    if (!validPassword) return res.status(400).json({message: "Password invalida"})
 
     res.status(200).json(respuesta)
   } catch(error) {
