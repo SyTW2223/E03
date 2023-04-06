2284c90ed77dc7fb74002f9670283e15a121bc84
AnaVGD <alu010132250@ull.edu.es>
2023-04-06 21:29:28 +0100

    Mejoras

---
+++ my-project_/src/views/CervezaView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Mi cerveza</h1>
+    <p>El título es: {{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CervezaView_',
+  data () {
+    return {
+      title: ''
+    }
+  },
+  mounted () {
+    fetch(process.env.VUE_APP_API_URL + '/api')
+      .then(res => res.json())
+      .then(res => {
+        this.title = res.title
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+}
+</script>
