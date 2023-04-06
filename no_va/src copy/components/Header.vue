7e969e0d75687604e23e0dc82745b24d34e31d93
AnaVGD <alu010132250@ull.edu.es>
2023-04-06 21:09:46 +0100

    Inicio

---
+++ no_va/src copy/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <nav>
+      <router-link :to="{ name: 'home' }">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/cerveza">Cerveza Place</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'VueHeader'
+}
+</script>
