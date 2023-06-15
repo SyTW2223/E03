45160cbc32c8025201e569d6e6b73ff648ce4421
Hector <alu0101365107@ull.edu.es>
2023-06-15 20:12:04 +0100

    Adicion test login error

--- my-project-server/test/testAPI.spec.js
+++ my-project-server/test/testAPI.spec.js
@@ -12,8 +12,16 @@ describe('Realizamos un login', () => {
         .post('/login')
         .send({email: '1', password: '1'})
         .end(function(err, res) {
-            console.log(res.body)
             chai.expect(res).to.have.status(200)
+            done()
+        })
+    })
+    it('Login Fallido', (done) => {
+        chai.request(url)
+        .post('/login')
+        .send({email: '1', password: '2'})
+        .end(function(err, res) {
+            chai.expect(res).to.have.status(400)
             done()
         })
     })
