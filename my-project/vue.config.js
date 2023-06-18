1193acd53ac66ce479803ed3e5774d49f1c04b7a
Hector <alu0101365107@ull.edu.es>
2023-06-18 22:29:44 +0100

    Configuracion github page

--- my-project/vue.config.js
+++ my-project/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/E03/" : "/",
   configureWebpack: {
     resolve: {
       fallback: {
